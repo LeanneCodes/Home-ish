c1505a336feac9b23f62e2d1c08607905dda6fa5
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-03-16 12:25:49 +0000

    create the product and category models

--- products/models.py
+++ products/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    website_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_website_name(self):
+        return self.website_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    width = models.CharField(max_length=254, null=True, blank=True)
+    height = models.CharField(max_length=254, null=True, blank=True)
+    depth = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
