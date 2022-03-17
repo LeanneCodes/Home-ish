24b200e23d8b1dda57e95d6837b12f56dff57fc7
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-03-17 10:21:37 +0000

    add add to bag functionality

--- bag/views.py
+++ bag/views.py
@@ -18,7 +18,6 @@ def add_to_bag(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
