cbd4d7a8da122d6027bf24294c1f57498d483e7f
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-03-17 16:26:20 +0000

    add stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KToQ3AfzoPYDcpGXkxXJP4HxaDRj0tYe9eQilg0Ob9QkmXlj8hhNkzPXY402weUch0QWdl7avJTmnMlKSWiXRpv004CgneTlG',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
