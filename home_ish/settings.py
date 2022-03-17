e6b63f1ff1dbc41c6aa3bf4b52640774dcf270ce
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-03-17 10:06:34 +0000

    add shopping bag app and contexts.py

--- home_ish/settings.py
+++ home_ish/settings.py
@@ -73,6 +73,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bag.contexts.bag_contents',
             ],
         },
     },
@@ -153,6 +154,9 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MINIMUM_SPEND_THRESHOLD = 350
+STANDARD_DELIVERY_PERCENTAGE = 20
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
