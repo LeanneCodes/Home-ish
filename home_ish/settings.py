e15b29153b90654fa6fd13d4a3d0400ca708c8f2
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-03-18 15:01:16 +0000

    create emails to send to users who place an order logged in

--- home_ish/settings.py
+++ home_ish/settings.py
@@ -176,6 +176,7 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'customercare@homeish.com'
 
 
 # Default primary key field type
