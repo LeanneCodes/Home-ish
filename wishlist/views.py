bf02b5799fd1f6969643aa3724c5ab112b678847
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-03-23 15:48:05 +0000

    add high res category photos for category homepage

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+
+from products.models import Product
 
 # Create your views here.
 
@@ -7,3 +10,15 @@ def wishlist(request):
     """ A view that shows visitors items they have favourited """
 
     return render(request, 'wishlist/wishlist.html')
+
+
+def add_to_wishlist(request, item_id):
+    """ Add items to wishlist page """
+
+    product = get_object_or_404(Product, pk=item_id)
+    redirect_url = request.POST.get('redirect_url')
+    wishlist = request.session.get('wishlist', {})
+    messages.success(request, f'Added {product.name} to your wishlist')
+
+    request.session['wishlist'] = wishlist
+    return redirect(redirect_url)
