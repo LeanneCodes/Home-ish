052fa4b4cc4edeb7f0bf1bd88eb979233882e9fd
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-04-04 10:14:40 +0000

    update order confirmation email to include discount applied if applicable

--- checkout/models.py
+++ checkout/models.py
@@ -46,7 +46,7 @@ class Order(models.Model):
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
             self.delivery_cost = 0
-        self.grand_total = self.order_total + self.delivery_cost
+        self.grand_total = self.order_total - self.delivery_cost - settings.DISCOUNT
         self.save()
 
     def save(self, *args, **kwargs):
