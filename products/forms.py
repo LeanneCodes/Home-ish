a592a97efb86f76359db5878ace9eb8e65e7c8ff
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-04-07 09:04:41 +0000

    add placeholders for admin control add a product page

--- products/forms.py
+++ products/forms.py
@@ -34,3 +34,28 @@ class ProductForm(forms.ModelForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black'
 
+        placeholders = {
+            'category': 'Category',
+            'category2': 'Sale or New Arrival?',
+            'sku': 'SKU',
+            'name': 'Name',
+            'description': 'Description',
+            'price': 'Original Price',
+            'currentprice': 'Current Price',
+            'rating': 'Rating',
+            'image_url': 'Image URL',
+            'image': 'Image',
+            'width': 'Width',
+            'height': 'Height',
+            'depth': 'Depth',
+        }
+
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
