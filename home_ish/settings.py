b9419626357c509ddf27fb58d7120a73b27c1d84
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-04-12 21:38:13 +0000

    remove secret key

--- home_ish/settings.py
+++ home_ish/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@k4zqoi^d*kg0bw4u9df=$5=)@wp2e7#rn8-tbh)e0y@n19d=('
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['homeish.herokuapp.com', 'localhost']
 
