75d9687a987209c04f321a38b6e80938a76f1a75
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-04-12 21:30:20 +0000

    connecting gitpod to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn home_ish.wsgi:application
--- home_ish/settings.py
+++ home_ish/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-@k4zqoi^d*kg0bw4u9df=$5=)@wp2e7#rn8-tbh)e0y@n19d=(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['homeish.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -132,12 +132,17 @@ WSGI_APPLICATION = 'home_ish.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
