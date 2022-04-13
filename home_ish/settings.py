c44eaddbca9a3e8ac4b0a74569242cebfff4ba06
Leanne Goldsmith <leannegoldsmith95@yahoo.com>
2022-04-13 11:15:24 +0000

    add email js

--- home_ish/settings.py
+++ home_ish/settings.py
@@ -222,7 +222,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'customercare@homeish.com'
+    DEFAULT_FROM_EMAIL = 'homeish.customercare@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
